8c1cc9666d75a1884bfa9c9eb34ef5dde0de306d
bee <610733719@qq.com>
2018-04-23 18:22:38 +0800

    日常提交

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+//func main(){
+//
+//	router := gin.Default()
+//
+//	router.GET("/get", func(c *gin.Context) {
+//		firstname := c.DefaultQuery("firstname", "Guest")
+//		lastname := c.Query("lastname")
+//		c.String(http.StatusOK,"Hello %s %s", firstname, lastname)
+//	})
+//	router.Run(":8000")
+//}
+
+func main(){
+	fmt.Println("hello,this is my first golang project!")
+}
